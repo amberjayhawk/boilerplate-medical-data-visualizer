2f3385b451acd222e7a29edaa62ec5a33a727da6
Amber Dawson <amberjayhawk@gmail.com>
2026-07-05 15:48:35 -0700

    sync work

--- medical_data_visualizer.py
+++ medical_data_visualizer.py
@@ -4,34 +4,47 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 # 1
-df = None
+df = pd.read_csv("medical_examination.csv")
 
 # 2
-df['overweight'] = None
+df["overweight"] = np.where((df["weight"] / ((df["height"] / 100) ** 2) > 25), 1, 0)
+df["overweight"]
+
 
 # 3
+
+df["choles_norm"] = np.where((df["cholesterol"] > 1), 1, 0)
+
+df["gluc_norm"] = np.where((df["gluc"] > 1), 1, 0)
 
 
 # 4
 def draw_cat_plot():
     # 5
-    df_cat = None
 
+    pd.melt(
+        df,
+        id_vars=[
+            ["choles_norm"],
+            ["gluc_norm"],
+            ["smoke"],
+            ["alco"],
+            ["active"],
+            ["cardio"],
+        ],
+        value_vars=["B"],
+    )
 
     # 6
     df_cat = None
-    
 
     # 7
-
-
 
     # 8
     fig = None
 
-
     # 9
-    fig.savefig('catplot.png')
+    fig.savefig("catplot.png")
     return fig
 
 
@@ -46,15 +59,11 @@ def draw_heat_map():
     # 13
     mask = None
 
-
-
     # 14
     fig, ax = None
 
     # 15
 
-
-
     # 16
-    fig.savefig('heatmap.png')
+    fig.savefig("heatmap.png")
     return fig
